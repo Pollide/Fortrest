7c1f11395ff6fe440d541712a6818e9ffff38b09
Allister Hamilton <allister.ham@gmail.com>
2023-03-12 20:43:37 +1300

    fixes

--- Fortrest/Assets/Artists/CoryTestAssets/Scripts/PlayerController.cs
+++ Fortrest/Assets/Artists/CoryTestAssets/Scripts/PlayerController.cs
@@ -233,7 +233,7 @@ public class PlayerController : MonoBehaviour
         if (Input.GetMouseButton(0) && Time.time > nextAttack && PlayerModeHandler.global.playerModes == PlayerModes.CombatMode && !PlayerModeHandler.global.MouseOverUI())
         {
             ChangeTool(new ToolData() { SwordBool = true });
-
+            VFXSlash.transform.position = SwordGameObject.transform.position;
             VFXSlash.Play();
             ApplyEnergyDamage(5.0f);
 
