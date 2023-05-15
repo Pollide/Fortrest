c988dd4ea68aa02a60d3af4ff92de6c185e6f155
V <Liam.vallesfin@gmail.com>
2023-05-15 23:24:47 +1200

    grass no shadows
    shadowsnotworking

--- Fortrest/Assets/Shaders/Includes/Lighting.hlsl
+++ Fortrest/Assets/Shaders/Includes/Lighting.hlsl
@@ -30,13 +30,9 @@ float GetLightIntensity(float3 color) {
 }
 
 #ifndef SHADERGRAPH_PREVIEW
-// This function gets additional light data and calculates realtime shadows
 Light GetAdditionalLightForToon(int pixelLightIndex, float3 worldPosition) {
-    // Convert the pixel light index to the light data index
     int perObjectLightIndex = GetPerObjectLightIndex(pixelLightIndex);
-    // Call the URP additional light algorithm. This will not calculate shadows, since we don't pass a shadow mask value
     Light light = GetAdditionalPerObjectLight(perObjectLightIndex, worldPosition);
-    // Manually set the shadow attenuation by calculating realtime shadows
     light.shadowAttenuation = AdditionalLightRealtimeShadow(perObjectLightIndex, worldPosition);
     return light;
 }
@@ -56,7 +52,6 @@ void AddAdditionalLights_float(float Smoothness, float3 WorldPosition, float3 Wo
 
     int pixelLightCount = GetAdditionalLightsCount();
     for (int i = 0; i < pixelLightCount; ++i) {
-        // In order to receive shadows in 2020.2, we must do a little extra than before. This function takes care of it
         Light light = GetAdditionalLightForToon(i, WorldPosition);
         half NdotL = saturate(dot(WorldNormal, light.direction));
         half atten = light.distanceAttenuation * light.shadowAttenuation * GetLightIntensity(light.color);
