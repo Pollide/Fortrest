a4b83c34b9aa4ee3799944adfaa7254252a7031d
FatalCrystal <88113430+FatalCrystal@users.noreply.github.com>
2023-06-01 12:57:07 +1200

    changes to ensure it doesnt spawn stuff ontop of stuff

--- Fortrest/Assets/ResourceGen/GenerateList.cs
+++ Fortrest/Assets/ResourceGen/GenerateList.cs
@@ -15,8 +15,6 @@ public class GenerateList
     public float maxY = 20;
     public Vector2 positionOnTerrain;
 
-
-
     public Vector3 CalculatePosition()
     {
         Terrain terrain = GameObject.FindObjectOfType<Terrain>();
@@ -57,6 +55,19 @@ public class GenerateList
                     i--;
                     continue;
                 }
+
+                if (!CheckMinDistance(randomPosition))
+                {
+                    stackOverflow--;
+                    if (stackOverflow <= 0)
+                    {
+                        // Debug.LogWarning("No terrain with parameters was found");
+                        return false;
+                    }
+                    i--;
+                    continue;
+                }
+
                 GameObject resource = PrefabUtility.InstantiatePrefab(resourcePrefab) as GameObject;
 
                 resource.transform.position = hit.point;
@@ -81,6 +92,60 @@ public class GenerateList
             if (resource.name.Replace("(Clone)", "") == resourcePrefab.name)
                 GameObject.DestroyImmediate(resource);
         }
+    }
+
+     //unity uses bit shifting to properly detect what layers should be masked, if the array is null, it will only see the "Default" layer.
+    public static int ReturnBitShift(string[] layerNameArray = null)
+    {
+        //array
+        if (layerNameArray == null)
+        {
+            layerNameArray = new string[] { "Default" };
+        }
+
+        int layerMaskInt = 0;
+
+        for (int i = 0; i < layerNameArray.Length; i++)
+        {
+            //bit shifting layers
+            layerMaskInt = layerMaskInt | 1 << LayerMask.NameToLayer(layerNameArray[i]);
+        }
+
+        return layerMaskInt;
+    }
+
+    private bool CheckCollision(Vector3 position)
+    {
+        Collider[] colliders = Physics.OverlapSphere(position, resourcePrefab.GetComponent<Collider>().bounds.size.magnitude);
+
+        foreach (Collider collider in colliders)
+        {
+            // Check if the collider belongs to the desired resource(s)
+            if (collider.CompareTag("Resource") || collider.gameObject.layer == LayerMask.NameToLayer("Resource"))
+            {
+                Debug.Log("Hit");
+                return true; // Collision detected with a resource
+            }
+        }
+
+        return false; // No collision with resources detected
+    }
+
+    private bool CheckMinDistance(Vector3 position)
+    {
+        GameObject[] spawnedObjects = GameObject.FindGameObjectsWithTag("Resource"); // Change the tag if necessary
+
+        foreach (GameObject spawnedObject in spawnedObjects)
+        {
+            float distance = Vector3.Distance(position, spawnedObject.transform.position);
+
+            if (distance < spawnedObject.GetComponent<SphereCollider>().bounds.size.magnitude / 2)
+            {
+                return false; // Distance is too close to an existing object
+            }
+        }
+
+        return true; // Minimum distance check passed
     }
 }
 
