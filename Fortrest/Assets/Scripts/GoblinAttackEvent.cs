9cd323c0fccc65b0d609596496b39d56d47aef44
Pollide <pol.idelon@mds.ac.nz>
2023-06-22 21:58:54 +1200

    Animation based attack

---
+++ Fortrest/Assets/Scripts/GoblinAttackEvent.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GoblinAttackEvent : MonoBehaviour
+{
+    public EnemyController script;
+
+    void Attack()
+    {
+        script.AnimationAttack();
+    }
+}
