3024a622a356d9d057d39cde2f7839a4266524cc
Awhesian <allister.ham@gmail.com>
2023-08-23 14:39:40 +1200

    bug fixes

--- Fortrest/Assets/Scripts/PopUpResource.cs
+++ Fortrest/Assets/Scripts/PopUpResource.cs
@@ -9,7 +9,7 @@ public class PopUpResource : MonoBehaviour
     private SpriteRenderer spriteRenderer;
     public TMP_Text amountText;
     public bool displayNow;
-    public int resourceInt;
+    public LevelManager.TierData TierData;
     private bool once = true;
 
     private void Awake()
@@ -27,39 +27,14 @@ public class PopUpResource : MonoBehaviour
         if (displayNow)
         {
             StopCoroutine("Hide");
-            switch (resourceInt)
-            {
-                case 0:
-                    spriteRenderer.sprite = LevelManager.global.WoodTierList[0].ResourceIcon;
-                    amountText.text = LevelManager.global.WoodTierList[0].ResourceAmount.ToString();
-                    break;
-                case 1:
-                    spriteRenderer.sprite = LevelManager.global.WoodTierList[1].ResourceIcon;
-                    amountText.text = LevelManager.global.WoodTierList[1].ResourceAmount.ToString();
-                    break;
-                case 2:
-                    spriteRenderer.sprite = LevelManager.global.WoodTierList[2].ResourceIcon;
-                    amountText.text = LevelManager.global.WoodTierList[3].ResourceAmount.ToString();
-                    break;
-                case 3:
-                    spriteRenderer.sprite = LevelManager.global.StoneTierList[0].ResourceIcon;
-                    amountText.text = LevelManager.global.StoneTierList[0].ResourceAmount.ToString();
-                    break;
-                case 4:
-                    spriteRenderer.sprite = LevelManager.global.StoneTierList[1].ResourceIcon;
-                    amountText.text = LevelManager.global.StoneTierList[1].ResourceAmount.ToString();
-                    break;
-                case 5:
-                    spriteRenderer.sprite = LevelManager.global.StoneTierList[2].ResourceIcon;
-                    amountText.text = LevelManager.global.StoneTierList[2].ResourceAmount.ToString();
-                    break;
-                default:
-                    break;
-            }
+
+            spriteRenderer.sprite = TierData.ResourceIcon;
+            amountText.text = TierData.ResourceAmount.ToString();
+
             if (spriteRenderer.color.a != 1.0f)
             {
                 GameManager.PlayAnimation(spriteRenderer.gameObject.GetComponent<Animation>(), "PopUpAppear");
-                GameManager.PlayAnimation(amountText.gameObject.GetComponent<Animation>(), "TextAppear");               
+                GameManager.PlayAnimation(amountText.gameObject.GetComponent<Animation>(), "TextAppear");
             }
             once = false;
             displayNow = false;
