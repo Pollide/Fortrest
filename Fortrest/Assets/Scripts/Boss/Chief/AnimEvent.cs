45908f16d5b6614d049ad2b35f37f5375c1351c2
FatalCrystal <88113430+FatalCrystal@users.noreply.github.com>
2023-09-27 08:18:32 +1300

    gg

--- Fortrest/Assets/Scripts/Boss/Chief/AnimEvent.cs
+++ Fortrest/Assets/Scripts/Boss/Chief/AnimEvent.cs
@@ -6,18 +6,43 @@ public class AnimEvent : MonoBehaviour
 {
     [SerializeField] private FirstAttackState attackState1;
     [SerializeField] private SecondAttackState attackState2;
+    [SerializeField] private ThirdAttackState attackState3;
     [SerializeField] private BossStateMachine stateMachine;
     [SerializeField] private BoxCollider bossCollider;
+    [SerializeField] private AudioClip slamAudio;
 
 
     void PlaySlashOne()
     {
         attackState1.PlaySlash(0);
+        GameManager.global.SoundManager.PlaySound(GameManager.global.SwordSwing1Sound);
     }
 
     void PlaySlashTwo()
     {
         attackState1.PlaySlash(1);
+        GameManager.global.SoundManager.PlaySound(GameManager.global.SwordSwing2Sound);
+    }
+
+    void PlaySlashThree()
+    {
+        attackState1.PlaySlash(2);
+        GameManager.global.SoundManager.PlaySound(GameManager.global.SwordSwing2Sound);
+    }
+
+    void PlaySwordRelease()
+    {
+        GameManager.global.SoundManager.PlaySound(GameManager.global.SwordSwing3Sound);
+    }
+
+    void PlaySlamSound()
+    {
+        GameManager.global.SoundManager.PlaySound(slamAudio);
+    }
+
+    void SetHasJumped()
+    {
+        attackState3.HasJumped = true;
     }
 
     void SetAttackFalse()
