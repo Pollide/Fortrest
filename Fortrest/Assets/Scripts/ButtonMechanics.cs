cf0ab17431ae2e633091d1cfe1cfea88936224df
Awhesian <allister.ham@gmail.com>
2023-09-30 22:30:31 +1300

    fixes

--- Fortrest/Assets/Scripts/ButtonMechanics.cs
+++ Fortrest/Assets/Scripts/ButtonMechanics.cs
@@ -161,11 +161,13 @@ public class ButtonMechanics : MonoBehaviour, IPointerUpHandler, IPointerDownHan
 
         if (DestroyBool)
         {
+            PlayerModeHandler.global.ReturnVFXBuilding(transform);
             PlayerModeHandler.global.SelectedTurret.DestroyBuilding();
         }
 
         if (RepairBool)
         {
+            PlayerModeHandler.global.ReturnVFXBuilding(transform);
             PlayerModeHandler.global.SelectedTurret.health = PlayerModeHandler.global.SelectedTurret.maxHealth;
             PlayerModeHandler.global.SelectedTurret.TakeDamage(0);
         }
