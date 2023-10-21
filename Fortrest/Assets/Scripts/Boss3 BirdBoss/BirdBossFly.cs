4b1f9697358a7ba5422d748a62c6a88e9ee04d1f
Pollide <pol.idelon@mds.ac.nz>
2023-10-22 12:54:06 +1300

    Bird Boss

--- Fortrest/Assets/Scripts/Boss3 BirdBoss/BirdBossFly.cs
+++ Fortrest/Assets/Scripts/Boss3 BirdBoss/BirdBossFly.cs
@@ -8,13 +8,13 @@ public class BirdBossFly : StateMachineBehaviour
     private float timer;
     private float attackCD = 2.0f;
     private bool targetSet;
-    private Vector3 destination;
     float x, z;
 
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
+    {       
         birdScript = animator.GetComponent<BirdBoss>();
+        birdScript.flyAnimOver = false;
         timer = 0f;
     }
 
@@ -25,17 +25,21 @@ public class BirdBossFly : StateMachineBehaviour
         {
             if (!birdScript.playerReached && birdScript.targetReached) //  && birdScript.distanceToPlayerNoY > birdScript.stoppingDistance
             {
-                birdScript.MoveToTarget(birdScript.playerTransform.position, birdScript.directionToPlayerNoY);
-                if (!birdScript.outOfScreen)
-                {                   
-                    if (birdScript.normalAttack)
+                birdScript.MoveToTarget(birdScript.playerTransform.position, birdScript.directionToPlayer);
+                if (!birdScript.IsOutOfScreen())
+                {
+                    if (birdScript.distanceToPlayer < 40.0f)
                     {
-                        animator.SetTrigger("Attack1");
-                    }
-                    else
-                    {
-                        animator.SetBool("Attack2", true);
-                        birdScript.normalAttack = true;
+                        if (birdScript.normalAttack)
+                        {
+
+                            animator.SetTrigger("Attack1");
+                        }
+                        else
+                        {
+                            animator.SetBool("Attack2", true);
+                            birdScript.normalAttack = true;
+                        }
                     }
                 }
             }    
@@ -47,10 +51,10 @@ public class BirdBossFly : StateMachineBehaviour
                     z = Random.Range(0, 2) == 0 ? Random.Range(40f, 150f) : Random.Range(-40f, -150f);
                     targetSet = true;
                 }
-                destination = birdScript.transform.position + new Vector3(x, 0f, z);
-                Vector3 directionToTarget = (new Vector3(destination.x, 0f, destination.z) - new Vector3(birdScript.transform.position.x, 0f, birdScript.transform.position.z)).normalized;
-                birdScript.MoveToTarget(destination, directionToTarget);
-                if (birdScript.outOfScreen)
+                birdScript.targetPosition = birdScript.transform.position + new Vector3(x, 0f, z);
+                birdScript.targetDirection = (new Vector3(birdScript.targetPosition.x, 0f, birdScript.targetPosition.z) - new Vector3(birdScript.transform.position.x, 0f, birdScript.transform.position.z)).normalized;
+                birdScript.MoveToTarget(birdScript.targetPosition, birdScript.targetDirection);
+                if (birdScript.IsOutOfScreen())
                 {
                     timer += Time.deltaTime;
                     if (timer > attackCD)
@@ -71,5 +75,6 @@ public class BirdBossFly : StateMachineBehaviour
         animator.ResetTrigger("Attack1");
         birdScript.playerReached = true;
         birdScript.targetReached = false;
+        birdScript.flyAnimOver = true;
     }
 }
