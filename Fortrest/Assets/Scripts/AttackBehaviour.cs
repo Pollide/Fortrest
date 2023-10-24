9ca2aa131ef70da761464bd0bd9f07866169d02e
Awhesian <allister.ham@gmail.com>
2023-10-24 18:45:13 +1300

    fix

--- Fortrest/Assets/Scripts/AttackBehaviour.cs
+++ Fortrest/Assets/Scripts/AttackBehaviour.cs
@@ -13,13 +13,13 @@ public class AttackBehaviour : StateMachineBehaviour
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        
+
     }
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        PlayerController.global.damageEnemy = false;
+        // PlayerController.global.damageEnemy = false;
         PlayerController.global.lunge = false;
         PlayerController.global.attackAnimEnded = true;
     }
