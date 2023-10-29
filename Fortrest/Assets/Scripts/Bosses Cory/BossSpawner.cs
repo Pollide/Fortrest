aa7e3f85477794e8c7e44a3d2c60c4f1a6c9529b
FatalCrystal <88113430+FatalCrystal@users.noreply.github.com>
2023-10-30 08:34:49 +1300

    Update BossSpawner.cs

--- Fortrest/Assets/Scripts/Bosses Cory/BossSpawner.cs
+++ Fortrest/Assets/Scripts/Bosses Cory/BossSpawner.cs
@@ -166,6 +166,7 @@ public class BossSpawner : MonoBehaviour
                 if (GetComponent<BossStateMachine>())
                 { 
                     bossAnimator.gameObject.SetActive(true);
+
                 }
                 else
                 {
