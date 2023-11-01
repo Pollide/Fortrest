5fd1c926ac4feb02f3cf9e4809fb459bff8e855c
FatalCrystal <88113430+FatalCrystal@users.noreply.github.com>
2023-11-01 17:22:45 +1300

    ff

--- Fortrest/Assets/Scripts/TransitionBridge/BridgeBuilder.cs
+++ Fortrest/Assets/Scripts/TransitionBridge/BridgeBuilder.cs
@@ -130,9 +130,6 @@ public class BridgeBuilder : MonoBehaviour
 
                     if (BridgeTypeInt == 1)
                     {
-
-                        manager.goblinSpawnable = true;
-                        manager.spawnEntries.Add(manager.goblin);
                         spawner.SpawnEnemies(manager.terrainDataList[BridgeTypeInt].terrain, manager.goblin.objectToSpawn, spawner.spawnMaxMarsh, ref spawner.spawnCurrentMarsh);
                     }
                     if (BridgeTypeInt == 2)
